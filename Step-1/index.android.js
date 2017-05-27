0f2acda86ceecad5f41a44767978393996e2cedf
farooqmajeed <farooq306@hotmail.com>
2017-05-27 16:50:22 +0500

    learning native

---
+++ Step-1/index.android.js
@@ -0,0 +1,56 @@
+// /**
+//  * Sample React Native App
+//  * https://github.com/facebook/react-native
+//  * @flow
+//  */
+
+// import React, { Component } from 'react';
+// import {
+//   AppRegistry,
+//   StyleSheet,
+//   Text,
+//   View
+// } from 'react-native';
+
+// export default class albums extends Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text style={styles.welcome}>
+//          farooq!
+//         </Text>
+      
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//     backgroundColor: '#F5FCFF',
+//   },
+//   welcome: {
+//     fontSize: 20,
+//     textAlign: 'center',
+//     margin: 10,
+//   },
+//   instructions: {
+//     textAlign: 'center',
+//     color: '#333333',
+//     marginBottom: 5,
+//   },
+// });
+
+// AppRegistry.registerComponent('albums', () => albums);
+
+import  React from 'react';
+import ReactNative from 'react-native';
+const App = () => {
+  return (
+    <text> farooq </text>  
+  );
+};
+ReactNative.AppRegistry.registerComponent('albums', () => App);
