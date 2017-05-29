ceed39010cff956a8d8dbdb93582e1828194f3ca
farooqmajeed <farooq306@hotmail.com>
2017-05-29 16:10:09 +0500

    learning native

--- step-2/src/components/AlbumList.js
+++ step-2/src/components/AlbumList.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axios from 'axios';
 
-const AlbumList = () => {
-    return (
-        <View>
-        <Text> AlbumList</Text>
-        </View>
-    );
-};
+class AlbumList extends Component {
+    state = { albums: [] };
+
+    componentWillMount() {
+        axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data }));
+    }
+
+    renderAlbums(){
+      return this.state.albums.map(album => <Text>{album.title}</Text>); 
+    }
+    render() {
+        return (
+            <View>
+              {this.renderAlbums()}
+            </View>
+        );
+    }
+}
 
 export default AlbumList;
