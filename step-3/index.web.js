b1d39cc3240754a6825a718883b5736979bd247c
farooqmajeed <farooq306@hotmail.com>
2017-05-30 15:22:07 +0500

    learning native

---
+++ step-3/index.web.js
@@ -0,0 +1,9 @@
+/* 
+    ./index.web.js
+*/
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './src/web/components/App.jsx';
+
+
+ReactDOM.render(<App  header= {'Album'} />, document.getElementById('root'));
