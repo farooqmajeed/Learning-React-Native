4ea6b4c3dc0f5e73df7b43971c6d99b17828d873
farooqmajeed <farooq306@hotmail.com>
2017-06-06 15:13:58 +0500

    learning native

--- step-4/src/components/header.js
+++ step-4/src/components/header.js
@@ -19,7 +19,7 @@ const styles = {
         alignItems: 'center',
         height: 60,
         paddingTop: 15,
-        shadowColor: '#000',
+        shadowColor: '#F8F8F8',
         shadowOffset: { width: 0, height:2 },
         shadowOpacity: 0.2,
         elevation: 2,
