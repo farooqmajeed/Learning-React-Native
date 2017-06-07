d0efb496626589fff1e0d2af99a0a77578c507db
farooqmajeed <farooq306@hotmail.com>
2017-06-07 15:00:47 +0500

    learning native

--- step-4/src/components/AlbumDetail.js
+++ step-4/src/components/AlbumDetail.js
@@ -24,7 +24,7 @@ const AlbumDetail = ({ album }) => {
             <CardSection>
             <Image
              style={imageStyle} 
-             source = {{ uri: image }}
+             source={{ uri: image }}
               />
             </CardSection>
         </Card>
