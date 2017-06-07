d76ff0bb7341db14745718350f6110dff8a017fb
farooqmajeed <farooq306@hotmail.com>
2017-06-07 14:53:34 +0500

    learning native

--- step-4/src/components/AlbumDetail.js
+++ step-4/src/components/AlbumDetail.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
-const AlbumDetail = (props) => {
+const AlbumDetail = ({ album }) => {
+
+    const { title, artist, thumbnail_image, image } = album;
+    const {thumbnailStyle, headerStyleContainer, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
     return (
         <Card>
             <CardSection>
-            
-                <Text>{props.album.title}</Text>
+                <View style={thumbnailContainerStyle} >
+                    <Image
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }}
+                        />
+                </View>
+                <View style={headerStyleContainer} >
+                    <Text style={headerTextStyle}>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardSection>
-        </Card> 
-    )
-
+            <CardSection>
+            <Image
+             style={imageStyle} 
+             source = {{ uri: image }}
+              />
+            </CardSection>
+        </Card>
+    );
 };
+
+const styles = {
+    headerStyleContainer: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+};
+
+
 
 export default AlbumDetail;
