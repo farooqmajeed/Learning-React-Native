223e3413f4cf20a5a835e79c13cff33563801083
farooqmajeed <farooq306@hotmail.com>
2017-06-15 15:54:06 +0500

    learning native

--- step-4/src/components/AlbumDetail.js
+++ step-4/src/components/AlbumDetail.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ({ album }) => {
 
-    const { title, artist, thumbnail_image, image } = album;
+    const { title, artist, thumbnail_image, image, url } = album;
     const {thumbnailStyle, headerStyleContainer, thumbnailContainerStyle, headerTextStyle, imageStyle } = styles;
     return (
         <Card>
@@ -29,11 +29,13 @@ const AlbumDetail = ({ album }) => {
                     />
             </CardSection>
             <CardSection>
-                <Button /> 
+                <Button  onPress ={() => Linking.openURL(url)} >
+                    Buy Now!
+                </Button> 
             </CardSection>
         </Card>
     );
-};
+}; 
 
 const styles = {
     headerStyleContainer: {
